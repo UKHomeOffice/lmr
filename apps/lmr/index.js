90d65652388cac1d7d603efb9d76af501834a812
Muhammed Islam <muhammed.islam@digital.homeoffice.gov.uk>
2024-01-12 19:18:04 +0000

    TLF-11: Add start page for LMR
    - add step for start page in index.js
    - add HTML template for new page, including links and custom button
    - add page content in pages.json

--- apps/lmr/index.js
+++ apps/lmr/index.js
@@ -1,23 +1,17 @@
+/* eslint-disable */
 'use strict';
 
-const hof = require('hof');
-const Submit = require('./behaviours/submit');
-const Summary = hof.components.summary;
+const SummaryPageBehaviour = require('hof').components.summary;
 
 module.exports = {
   name: 'lmr',
   steps: {
-    '/privacy': {
-      behaviours: [Summary, Submit],
-      fields: ['privacy-check'],
-      sections: require('./sections/summary-data-sections'),
-      backLink: true,
-      next: '/privacy-declaration'
+    '/start': {
+      next: '/summary'
     },
-    '/privacy-declaration': {
-      behaviours: ['complete'],
-      backLink: false,
-      clearSession: true
+    '/summary': {
+      behaviours: [SummaryPageBehaviour],
+      sections: require('./sections/summary-data-sections')
     }
   }
 };
