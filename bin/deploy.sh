06c92dc638598b500c042acfb978f091bd21aa29
JHoldergov <125476151+JHoldergov@users.noreply.github.com>
2024-01-29 17:48:19 +0000

    branch final deployment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -19,7 +19,7 @@ if [[ $1 == 'tear_down' ]]; then
 
   $kd --delete -f kube/configmaps/configmap.yml
   $kd --delete -f kube/html-pdf -f kube/app
-  echo "Torn Down UAT Branch - lmr-$DRONE_SOURCE_BRANCH.internal.sas-notprod.homeoffice.gov.uk"
+  echo "Torn Down UAT Branch - lmr-$DRONE_SOURCE_BRANCH.internal.sas-lmr-branch.homeoffice.gov.uk"
   exit 0
 fi
 
@@ -49,6 +49,6 @@ fi
 sleep $READY_FOR_TEST_DELAY
 
 if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
-  echo "App Branch - lmr-$DRONE_SOURCE_BRANCH.internal.sas-notprod.homeoffice.gov.uk"
+  echo "App Branch - lmr-$DRONE_SOURCE_BRANCH.internal.sas-lmr-branch.homeoffice.gov.uk"
   echo "Data Service Branch - data-service-$DRONE_SOURCE_BRANCH.sas-lmr-branch.homeoffice.gov.uk"
 fi
