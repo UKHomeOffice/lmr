44b3a7fdf1b785bb1eaadedae2a058bb6dc3f277
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2025-08-14 15:57:28 +0100

    LMR-14 simplified line

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,8 @@
 'use strict';
 
 (async () => {
-  const chai = await import('chai');
+  global.chai = await import('chai');
 
-  global.chai = chai;
   global.should = chai.should();
   global.expect = chai.expect;
 })();
